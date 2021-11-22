a08786e091799520d6298c2a2f5fa869d20a5cf6
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-22 18:14:25 +0530

    feat: add leaderboard route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 require('dotenv').config();
 require('./models/dbinit');
 import express, {Request, Response} from 'express'
+import leaderBoardRouter from './routes/leaderboard';
 
 
 const app: express.Application = express();
 const PORT: string = process.env.PORT!;
 
+
+app.use('/leaderboard', leaderBoardRouter);
 app.get('/', (req: Request, res: Response ) =>{
     res.send("Hello world!");
 });
