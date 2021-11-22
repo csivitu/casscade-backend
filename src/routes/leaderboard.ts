7aedf8970c6aa676704f2bcfc36755c374695f0a
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-23 01:47:50 +0530

    feat: send sorted data

--- src/routes/leaderboard.ts
+++ src/routes/leaderboard.ts
@@ -5,7 +5,7 @@ import Submissions from '../models/submission';
 const router: express.Router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const submission = await Submissions.find({});
+    const submission = await Submissions.find().sort({"score":-1});
     if(!submission){
         res.status(200);
         res.json({error: true, message: "no response from DB"});
