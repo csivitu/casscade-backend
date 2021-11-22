6539497f4af006681d365e7d56177ebb02846c92
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-22 14:00:33 +0530

    feat: init commit

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+require('dotenv').config();
+import express, {Request, Response} from 'express'
+
+
+const app: express.Application = express();
+const PORT: string = process.env.PORT!;
+
+app.get('/', (req: Request, res: Response ) =>{
+    res.send("Hello world!");
+});
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+});
