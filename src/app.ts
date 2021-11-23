13de898794426e4378d780d28296fa40af3f3e8b
Shiv10 <shivanshsharma2012@gmail.com>
2021-11-23 12:58:20 +0530

    feat: add cors access

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,18 @@ require('dotenv').config();
 require('./models/dbinit');
 import express, {Request, Response} from 'express'
 import leaderBoardRouter from './routes/leaderboard';
+import cors from 'cors';
 
 
 const app: express.Application = express();
 const PORT: string = process.env.PORT!;
+const options = {
+    origin: ['http://localhost:3000', 'https://casscade.csivit.com'], 
+    methods: ['GET'],
+    allowedHeaders: ['Content-Type'],
+};
 
-
+app.use(cors(options))
 app.use('/leaderboard', leaderBoardRouter);
 app.get('/', (req: Request, res: Response ) =>{
     res.send("Hello world!");
